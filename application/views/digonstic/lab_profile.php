e17f2d9c9bb513993c7c61e93da58c6cb422f670
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-11-05 17:31:14 +0530

    list search
    commit

--- application/views/digonstic/lab_profile.php
+++ application/views/digonstic/lab_profile.php
@@ -1,4 +1,3 @@
-
 <!--Main layout-->
 <main class="">
 
@@ -17,36 +16,42 @@
                         <div class="card-body pb-0">
 
                             <div class="media d-block d-md-flex">
-							<?php 
+                                <?php 
 								$mail_url=$this->config->item('lab_url');
 								if($lab_deatils['profile_pic']==''){ ?>
-									<img class="d-flex mb-3 mx-auto z-depth-1" src="<?php echo base_url('assets/profile_pic/dia-test.png'); ?>" alt="Logo" style="width: 100px;">
-								<?php }else{ ?>
-								<img class="d-flex mb-3 mx-auto z-depth-1" src="<?php echo $mail_url.'assets/profile_pic/'.$lab_deatils['profile_pic']; ?>" alt="<?php echo isset($lab_deatils['profile_pic'])?$lab_deatils['profile_pic']:''; ?>" style="width: 100px;">
-								<?php } ?>
+                                <img class="d-flex mb-3 mx-auto z-depth-1" src="<?php echo base_url('assets/profile_pic/dia-test.png'); ?>" alt="Logo" style="width: 100px;">
+                                <?php }else{ ?>
+                                <img class="d-flex mb-3 mx-auto z-depth-1" src="<?php echo $mail_url.'assets/profile_pic/'.$lab_deatils['profile_pic']; ?>" alt="<?php echo isset($lab_deatils['profile_pic'])?$lab_deatils['profile_pic']:''; ?>" style="width: 100px;">
+                                <?php } ?>
                                 <div class="media-body text-center text-md-left ml-md-3 ml-0">
-                                    <h4 class="mt-0 font-weight-bold"><?php echo isset($lab_deatils['name'])?$lab_deatils['name']:''; ?></h4>
+                                    <h4 class="mt-0 font-weight-bold">
+                                        <?php echo isset($lab_deatils['name'])?$lab_deatils['name']:''; ?>
+                                    </h4>
                                     <p class="mb-0">
-									<?php echo isset($lab_deatils['address1'])?$lab_deatils['address1']:''; ?>,
-									<?php echo isset($lab_deatils['address2'])?$lab_deatils['address2']:''; ?>
-									<?php echo isset($lab_deatils['city'])?$lab_deatils['city']:''; ?>	
-									<?php echo isset($lab_deatils['state'])?$lab_deatils['state']:''; ?>	
-									<?php echo isset($lab_deatils['country'])?$lab_deatils['country']:''; ?></p>	
-                                    <p>Zip code : <?php echo isset($lab_deatils['zipcode'])?$lab_deatils['zipcode']:''; ?></p>
+                                        <?php echo isset($lab_deatils['address1'])?$lab_deatils['address1']:''; ?>,
+                                        <?php echo isset($lab_deatils['address2'])?$lab_deatils['address2']:''; ?>
+                                        <?php echo isset($lab_deatils['city'])?$lab_deatils['city']:''; ?>
+                                        <?php echo isset($lab_deatils['state'])?$lab_deatils['state']:''; ?>
+                                        <?php echo isset($lab_deatils['country'])?$lab_deatils['country']:''; ?>
+                                    </p>
+                                    <p>Zip code :
+                                        <?php echo isset($lab_deatils['zipcode'])?$lab_deatils['zipcode']:''; ?>
+                                    </p>
                                 </div>
                             </div>
 
                             <div class="mt-3 d-block d-md-flex">
-							<?php if(isset($lab_deatils['test_names']) && count($lab_deatils['test_names'])>0){ ?>
+                                <?php if(isset($lab_deatils['test_names']) && count($lab_deatils['test_names'])>0){ ?>
                                 <div>
                                     <label class="mb-0">Facilities</label>
                                     <ul class="list-inline list-styled">
-									<?php foreach($lab_deatils['test_names'] as $list){ ?>
-                                        <li class="list-inline-item"><span>&#8226;</span> <small><?php echo isset($list['test_name'])?$list['test_name']:''; ?></small></li>
-									<?php } ?>
-                                        </ul>
+                                        <?php foreach($lab_deatils['test_names'] as $list){ ?>
+                                        <li class="list-inline-item"><span>&#8226;</span> <small>
+                                                <?php echo isset($list['test_name'])?$list['test_name']:''; ?></small></li>
+                                        <?php } ?>
+                                    </ul>
                                 </div>
-								<?php } ?>
+                                <?php } ?>
                                 <!--<div class="ml-5">
                                     <label class="mb-0">Next Slot</label>
                                     <ul class="list-inline list-styled">
@@ -60,32 +65,135 @@
                         <hr>
 
                         <!--Card content-->
-						<?php if(isset($lab_deatils['test_names']) && count($lab_deatils['test_names'])>0){ ?>
+                        <?php if(isset($lab_deatils['test_names']) && count($lab_deatils['test_names'])>0){ ?>
                         <div class="card-body">
 
                             <div class="">
                                 <h5 class="mt-0 font-weight-bold">All Tests</h5>
-                                                           <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
+                                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
 
                             </div>
 
                             <hr>
 
-						<ul id="myUL">
-						  <li><a href="#">Adele</a></li>
-						  <li><a href="#">Agnes</a></li>
+                            <ul id="myUL">
+                                <li>
+                                    <hr class="mt-0">
 
-						  <li><a href="#">Billy</a></li>
-						  <li><a href="#">Bob</a></li>
+                                    <div class="d-block d-md-flex pb-0">
+                                        <div class="mt-2">
+                                            <h6 class="mb-1">REMO - AMLI / ETO IN AML M - 2</h6>
+                                            <small>Also known as: Amli/Eto In Aml M2 Fish Blood.</small>
+                                        </div>
+                                        <div class="ml-auto">
+                                            <div class="d-block d-md-flex">
+                                                <p class="mb-0 font-weight-bold mt-2"><span>&#8377;</span><span>8561</span></p>
+                                                <a id="" class="btn btn-outline-primary btn-sm ml-5" href="#" role="button">Book</a>
+                                            </div>
+                                            <div class="">
+                                                <ul class="list-unstyled">
+                                                    <li class=""><span>&#8226;</span> <small>Lorem ipsum</small></li>
+                                                    <li class=""><span>&#8226;</span> <small>Phasellus iaculis</small></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <hr class="mt-0">
 
-						  <li><a href="#">Calvin</a></li>
-						  <li><a href="#">Christina</a></li>
-						  <li><a href="#">Cindy</a></li>
-						</ul>
+                                    <div class="d-block d-md-flex pb-0">
+                                        <div class="mt-2">
+                                            <h6 class="mb-1">AVATAR - AMLI / ETO IN AML M - 2</h6>
+                                            <small>Also known as: Amli/Eto In Aml M2 Fish Blood.</small>
+                                        </div>
+                                        <div class="ml-auto">
+                                            <div class="d-block d-md-flex">
+                                                <p class="mb-0 font-weight-bold mt-2"><span>&#8377;</span><span>8561</span></p>
+                                                <a id="" class="btn btn-outline-primary btn-sm ml-5" href="#" role="button">Book</a>
+                                            </div>
+                                            <div class="">
+                                                <ul class="list-unstyled">
+                                                    <li class=""><span>&#8226;</span> <small>Lorem ipsum</small></li>
+                                                    <li class=""><span>&#8226;</span> <small>Phasellus iaculis</small></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <hr class="mt-0">
+
+                                    <div class="d-block d-md-flex pb-0">
+                                        <div class="mt-2">
+                                            <h6 class="mb-1">RATIO - AMLI / ETO IN AML M - 2</h6>
+                                            <small>Also known as: Amli/Eto In Aml M2 Fish Blood.</small>
+                                        </div>
+                                        <div class="ml-auto">
+                                            <div class="d-block d-md-flex">
+                                                <p class="mb-0 font-weight-bold mt-2"><span>&#8377;</span><span>8561</span></p>
+                                                <a id="" class="btn btn-outline-primary btn-sm ml-5" href="#" role="button">Book</a>
+                                            </div>
+                                            <div class="">
+                                                <ul class="list-unstyled">
+                                                    <li class=""><span>&#8226;</span> <small>Lorem ipsum</small></li>
+                                                    <li class=""><span>&#8226;</span> <small>Phasellus iaculis</small></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <hr class="mt-0">
+
+                                    <div class="d-block d-md-flex pb-0">
+                                        <div class="mt-2">
+                                            <h6 class="mb-1">REVENGE - AMLI / ETO IN AML M - 2</h6>
+                                            <small>Also known as: Amli/Eto In Aml M2 Fish Blood.</small>
+                                        </div>
+                                        <div class="ml-auto">
+                                            <div class="d-block d-md-flex">
+                                                <p class="mb-0 font-weight-bold mt-2"><span>&#8377;</span><span>8561</span></p>
+                                                <a id="" class="btn btn-outline-primary btn-sm ml-5" href="#" role="button">Book</a>
+                                            </div>
+                                            <div class="">
+                                                <ul class="list-unstyled">
+                                                    <li class=""><span>&#8226;</span> <small>Lorem ipsum</small></li>
+                                                    <li class=""><span>&#8226;</span> <small>Phasellus iaculis</small></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <hr class="mt-0">
+
+                                    <div class="d-block d-md-flex pb-0">
+                                        <div class="mt-2">
+                                            <h6 class="mb-1">NOTHING - AMLI / ETO IN AML M - 2</h6>
+                                            <small>Also known as: Amli/Eto In Aml M2 Fish Blood.</small>
+                                        </div>
+                                        <div class="ml-auto">
+                                            <div class="d-block d-md-flex">
+                                                <p class="mb-0 font-weight-bold mt-2"><span>&#8377;</span><span>8561</span></p>
+                                                <a id="" class="btn btn-outline-primary btn-sm ml-5" href="#" role="button">Book</a>
+                                            </div>
+                                            <div class="">
+                                                <ul class="list-unstyled">
+                                                    <li class=""><span>&#8226;</span> <small>Lorem ipsum</small></li>
+                                                    <li class=""><span>&#8226;</span> <small>Phasellus iaculis</small></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
 
 
                         </div>
-						<?php } ?>
+                        <?php } ?>
 
                     </div>
 
@@ -156,28 +264,26 @@
 <!--Main layout-->
 
 <script>
-function addtocard(id,l_id){
-	alert(id);
-	alert(l_id);
-	
-}
+    function addtocard(id, l_id) {
+        alert(id);
+        alert(l_id);
 
-
+    }
 </script>
 <script>
-function myFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+    function myFunction() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
         }
     }
-}
 </script>
