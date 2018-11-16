7ae2a8425b1553037febd64b6134be3f8584b419
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-11-16 14:33:01 +0530

    adding  cart page
    adding  cart page

--- application/controllers/Diagnostic.php
+++ application/controllers/Diagnostic.php
@@ -499,9 +499,9 @@ class Diagnostic extends In_frontend {
 		{
 			$log_details=$this->session->userdata('app_user');
 			$package_id=base64_decode($this->uri->segment(3));
-			$data['order_list']=$this->Diagnostic_model->get_package_details($package_id);
+			//$data['order_list']=$this->Diagnostic_model->get_package_details($package_id);
 			//echo '<pre>';print_r($data);exit;
-			$this->load->view('digonstic/cart',$data);
+			$this->load->view('digonstic/cart');
 			$this->load->view('html/footer');
 			//echo '<pre>';print_r($order_id);exit;
 		}else{
